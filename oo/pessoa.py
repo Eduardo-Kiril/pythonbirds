8480ccddbe48f3799dcd45543083b338a31a9314
edrop <778899Sandra!778899>
2021-06-25 15:47:58 -0300

    criada a variavel filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=18):
+    def __init__(self, *filhos, nome=None, idade=18):
             self.idade = idade
             self.nome = nome
+            self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Kiril')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Dogão'
-    print(p.nome)
+    jota = Pessoa(nome='Jota')
+    kiril = Pessoa(jota, nome='Kiril')
+    print(Pessoa.cumprimentar(kiril))
+    print(id(kiril))
+    print(kiril.cumprimentar())
+    print(kiril.nome)
+    print(kiril.idade)
+    for filho in kiril.filhos:
+        print(filho.nome)
+
