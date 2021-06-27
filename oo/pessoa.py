6b80558210076e626f473ace3b87a446d4cfe7cb
edrop <778899Sandra!778899>
2021-06-27 17:33:12 -0300

    criada a classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=18):
             self.idade = idade
             self.nome = nome
@@ -16,4 +18,11 @@ if __name__ == '__main__':
     print(kiril.idade)
     for filho in kiril.filhos:
         print(filho.nome)
-
+    kiril.sobrenome = "Rodrigues"
+    kiril.olhos = 1
+    Pessoa.olhos = 3
+    print(kiril.__dict__)
+    print(jota.__dict__)
+    print(Pessoa.olhos)
+    print(kiril.olhos)
+    print(jota.olhos)
